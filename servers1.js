31a5364d71d34c126dfa1637bc998f0013343ebd
XxMUH4MM4DxX <diegoamanecer624@gmail.com>
2025-02-24 17:41:55 -0500

    Add files via upload

---
+++ servers1.js
@@ -0,0 +1,14 @@
+import { createServer } from 'node:http';
+
+const hostname ='127.0.0.1';
+const port = 3000;
+
+const server = createServer((req, res => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.readableEnded('Hello World');
+}));
+
+server.listen(port, hostname, () => {
+    console.log(`Servidor corriendo en http://${hostname}:${port}/`);
+})
